6167ff69b860a55584bcca3a2ad9f0f13dd19433
Ukpono Obott <37601569+Ukponobott@users.noreply.github.com>
2021-05-15 01:40:07 +0100

    Updated runtime.txt and index file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Welcome to Fashion House"
+    return render_template("index.html")
 
 
 @app.route("/login")
