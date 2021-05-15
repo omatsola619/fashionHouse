a8f1b0d33584e3729501fe4868ee4912368ef1bc
Ukpono Obott <37601569+Ukponobott@users.noreply.github.com>
2021-05-15 01:51:56 +0100

    Updated app file to correct loading errors

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 
 app = Flask(__name__)
@@ -34,4 +35,5 @@ def store():
 
 
 if __name__ == "__main__":
-    app.run(port=2500, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
